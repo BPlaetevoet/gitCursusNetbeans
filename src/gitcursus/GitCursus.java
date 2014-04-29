c8e1db0c3d34199eaf68c7883bc408b4eb6b4ac2
User_2 <User_2@lok303>
2014-04-29 09:15:55 +0200

    eerste

---
+++ src/gitcursus/GitCursus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitcursus;
+
+/**
+ *
+ * @author User_2
+ */
+public class GitCursus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
